f04d5d098ec226d1b2923c0767e3e5738a40f5de
AMAN125313002 <125313002@nitkkr.ac.in>
2025-11-25 16:33:42 +0530

    01_find_largest_element.c

--- C-programming-collection-master/Arrays/01_find_largest_element.c
+++ C-programming-collection-master/Arrays/01_find_largest_element.c
@@ -4,7 +4,7 @@ int main() {
     int n, i, largest;
     int arr[100];
     
-    printf("Siddharth Koshav - 125113006\n");
+    printf("AMAN MISHRA - 125313002\n");
     printf("Enter number of elements: ");
     scanf("%d", &n);
     
