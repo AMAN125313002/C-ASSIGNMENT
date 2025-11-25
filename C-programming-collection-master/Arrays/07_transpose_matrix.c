e581b142d2fa1a02b504997558692ddd3de08e6e
AMAN125313002 <125313002@nitkkr.ac.in>
2025-11-25 16:37:46 +0530

    07_transpose_matrix.c

--- C-programming-collection-master/Arrays/07_transpose_matrix.c
+++ C-programming-collection-master/Arrays/07_transpose_matrix.c
@@ -4,7 +4,7 @@ int main() {
     int rows, cols, i, j;
     int matrix[10][10], transpose[10][10];
     
-    printf("Siddharth Koshav - 125113006\n");
+    printf("AMAN MISHRA - 125313002\n");
     printf("Enter number of rows and columns: ");
     scanf("%d %d", &rows, &cols);
     
