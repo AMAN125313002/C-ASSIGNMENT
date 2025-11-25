9e8b987d729c002c902b848d768e41cbc4612ee6
AMAN125313002 <125313002@nitkkr.ac.in>
2025-11-25 17:19:14 +0530

    Update 08_sort_by_roll.c

--- C-programming-collection-master/Structures/08_sort_by_roll.c
+++ C-programming-collection-master/Structures/08_sort_by_roll.c
@@ -10,7 +10,7 @@ int main() {
     int n, i, j;
     struct Student students[100], temp;
     
-    printf("Siddharth Koshav - 125113006\n");
+    printf("AMAN MISHRA - 125313002\n");
     printf("Enter number of students: ");
     scanf("%d", &n);
     
